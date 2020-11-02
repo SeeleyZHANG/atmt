ccabde750ba4cdabf7c13883c05256b5017489e3
SeeleyZHANG <65488069+SeeleyZHANG@users.noreply.github.com>
2020-11-02 10:26:50 +0100

    bpe code

--- code/bpe.sh
+++ code/bpe.sh
@@ -4,6 +4,6 @@ bash preprocess_bpe_data.sh
 
 
 python train.py --batch-size 100
-python translate.py --data baseline/prepared_data/
+python translate.py --data bpe/prepared_data/
 bash postprocess.sh model_translations.txt model_translations.out en 
-cat model_translations.out | sacrebleu baseline/raw_data/test.en
+cat model_translations.out | sacrebleu bpe/raw_data/test.en
